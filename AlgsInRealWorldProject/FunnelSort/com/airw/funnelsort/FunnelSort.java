f19580e3900753d1825f5ebf671e17e1eea92c57
Grant <Grant@128.237.253.95>
2014-04-17 19:11:31 -0400

    Println funnel sort

--- AlgsInRealWorldProject/FunnelSort/com/airw/funnelsort/FunnelSort.java
+++ AlgsInRealWorldProject/FunnelSort/com/airw/funnelsort/FunnelSort.java
@@ -3,8 +3,7 @@ package com.airw.funnelsort;
 public class FunnelSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Funnel Sort");
 	}
 
 }
